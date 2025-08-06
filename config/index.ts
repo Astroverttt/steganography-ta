ab6443be6c8fd671177f5193068ced98b31b14d6
Muhammad Farel Aryasatya <farelxplay12@gmail.com>
2025-08-06 16:32:47 +0700

    Add api base url to config

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,2 @@
-const BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000";
+const BASE_URL = process.env.NEXT_BASE_URL || "http://localhost:8000";
 export default BASE_URL;
